b6f0b2c72de03dedfdb530cfe7e58d752c90a6b8
Well-Sonn <wellerson819@gmail.com>
2023-10-24 19:27:47 -0300

    subindo projeto fetch testado

---
+++ readme.md
@@ -0,0 +1,38 @@
+# **API**
+
+Este é um script Node.js que utiliza a biblioteca Axios para fazer uma requisição à API do BlockScout Ethereum Explorer. Ele busca transações associadas a um endereço Ethereum específico nas últimas 24 horas, formata os dados relevantes e os salva em um arquivo JSON. Com isso, é possível monitorar as transações recentes associadas ao endereço Ethereum fornecido.
+
+### **Pré-requisitos**
+
+Antes de executar este script, certifique-se de ter o Node.js instalado em seu sistema. 
+
+Além disso, é necessário instalar as dependências do projeto. No terminal, navegue até o diretório do projeto e execute o seguinte comando:
+
+```
+npm install axios
+```
+
+Como usar
+Clone este repositório em seu sistema local:
+
+```
+git clone <URL_do_repositorio>
+```
+
+No arquivo script.js, substitua *'seu diretorio aqui'* pela localização onde você deseja salvar o arquivo JSON gerado.
+
+No terminal, navegue até o diretório do projeto e execute o script usando o seguinte comando:
+
+```
+node script.js
+```
+
+O script realizará a requisição à API, filtrará as transações das últimas 24 horas e salvará os dados formatados no arquivo JSON especificado.
+
+## **Personalização**
+
+Se desejar monitorar um endereço Ethereum diferente, altere o URL da API na linha:
+
+const response = await axios.get('https://eth.blockscout.com/api/v2/addresses 0x25D43E7c258bA2c971A146e0a753Cd5273df9559/transactions');
+
+Substitua 0x25D43E7c258bA2c971A146e0a753Cd5273df9559 pelo endereço Ethereum desejado, esse enderço é apenas um endereço publico fornecido no site do blockscout.
---
+++ script.js
@@ -0,0 +1,48 @@
+const axios = require('axios');
+const fs = require('fs');
+
+async function fetchAndSaveData() {
+    const agora = new Date();
+    agora.setDate(agora.getDate() - 1);
+    console.log(agora);
+
+    try {
+        const response = await axios.get('https://eth.blockscout.com/api/v2/addresses/0x25D43E7c258bA2c971A146e0a753Cd5273df9559/transactions');
+        const transactions = response.data.items;
+        
+
+
+        const transacoesUltimas24h = transactions.filter(transaction => {
+            const timestamp = new Date(transaction.timestamp);
+            return timestamp > agora;
+        });
+
+        const transacoesFormatadas = transacoesUltimas24h.map(transaction => {
+            return {
+                hash: transaction.hash,
+                from: transaction.from.hash,
+                to: transaction.to.hash,
+                valor: transaction.value,
+                data: new Date(transaction.timestamp).toLocaleDateString(),
+                hora: new Date(transaction.timestamp).toLocaleTimeString(),
+                descricao: transaction.tx_types.join(', ')
+            };
+        });
+
+        const jsonData = JSON.stringify(transacoesFormatadas, null, 2);
+        const outputFilePath = 'seu diretorio aqui';
+
+        fs.writeFileSync(outputFilePath, jsonData, (err) => {
+            if (err) {
+                console.error('Erro ao salvar o arquivo:', err);
+            } else {
+                console.log('Dados salvos com sucesso em:', outputFilePath);
+            }
+        });
+    } catch (error) {
+        console.error('Erro ao buscar dados da API:', error);
+    }
+}
+
+// Chama a função para coletar e salvar os dados
+fetchAndSaveData();
